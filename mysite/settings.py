0352153ee131c76f66a7371d6c222e9c2ee0c964
Padley <perardpadley245@gmail.com>
2023-10-03 00:39:57 -0400

    Modified templates to displayposts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-##^8bf(+js7980!vf+7nim6cex0$-c!m$n62wxj#b6r_f=-0dx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
